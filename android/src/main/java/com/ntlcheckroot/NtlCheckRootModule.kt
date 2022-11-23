24b1a3abce492bc3e6830e344ddc971c575fd96f
Ratchanan Sakdiyotintada <ratchanan.sak@gmail.com>
2022-11-23 19:14:00 +0700

    feat: add detect by suspect library list

--- android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
+++ android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
@@ -1,9 +1,11 @@
 package com.ntlcheckroot
 
+import android.content.pm.PackageManager
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 import com.scottyab.rootbeer.RootBeer
+import java.io.File
 
 class NtlCheckRootModule internal constructor(context: ReactApplicationContext) :
   NtlCheckRootSpec(context) {
@@ -14,11 +16,34 @@ class NtlCheckRootModule internal constructor(context: ReactApplicationContext) 
 
   @ReactMethod
   override fun checkRootJail(promise: Promise) {
-    var rootBeer = RootBeer(reactApplicationContext)
-    promise.resolve(rootBeer.isRooted())
+    val rootBeer = RootBeer(reactApplicationContext)
+    var isSuspect = false
+    val pm = reactApplicationContext.getPackageManager()
+    val packages = pm.getInstalledPackages(0)
+
+    for (packageInfo in packages) {
+      val appInfo = packageInfo.applicationInfo
+      val suspectPaths = SUSPECT_LIBRARYS.map {
+        File(appInfo.nativeLibraryDir + it)
+      }
+
+      isSuspect = suspectPaths.any {
+        it.exists()
+      }
+    }
+
+    promise.resolve(rootBeer.isRooted() || isSuspect)
   }
 
   companion object {
     const val NAME = "NtlCheckRoot"
+    private val SUSPECT_LIBRARYS = listOf(
+      "/libmagisk64.so",
+      "/libmagiskinit.so",
+      "/libmagisk32.so",
+      "/libbusybox.so",
+      "/libmagiskboot.so",
+      "/libstub.so",
+    )
   }
 }
