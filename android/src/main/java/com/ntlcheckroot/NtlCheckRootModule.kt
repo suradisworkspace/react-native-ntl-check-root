1f44de22e5052430de4eff01450eb584ddcfc0f7
Ratchanan Sakdiyotintada <ratchanan.sak@gmail.com>
2022-11-29 20:41:25 +0700

    fix: add break loop

--- android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
+++ android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
@@ -19,22 +19,26 @@ class NtlCheckRootModule internal constructor(context: ReactApplicationContext) 
     val rootBeer = RootBeer(reactApplicationContext)
     val isOnEmulator = Build.FINGERPRINT.contains("generic", ignoreCase = true) || 
       Build.DEVICE.contains("generic", ignoreCase = true)
-    var isSuspect = false
+    var isSuspicious = false
     val pm = reactApplicationContext.getPackageManager()
     val packages = pm.getInstalledPackages(0)
 
     for (packageInfo in packages) {
       val appInfo = packageInfo.applicationInfo
-      val suspectPaths = SUSPECT_LIBRARYS.map {
+      val suspiciousPaths = SUSPECT_LIBRARYS.map {
         File(appInfo.nativeLibraryDir + it)
       }
 
-      isSuspect = suspectPaths.any {
+      isSuspicious = suspiciousPaths.any {
         it.exists()
+      }
+
+      if (isSuspicious) {
+        break
       }
     }
 
-    promise.resolve(isOnEmulator || rootBeer.isRooted() || isSuspect)
+    promise.resolve(isOnEmulator || rootBeer.isRooted() || isSuspicious)
   }
 
   companion object {
