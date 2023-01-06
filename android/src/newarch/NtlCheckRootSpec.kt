d0f56453024b30ddd048cdcaec485d7301bc2f2a
Ratchanan Sakdiyotintada <ratchanan.sak@gmail.com>
2023-01-06 14:54:43 +0700

    fix: add import

--- android/src/newarch/NtlCheckRootSpec.kt
+++ android/src/newarch/NtlCheckRootSpec.kt
@@ -1,6 +1,7 @@
 package com.ntlcheckroot
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.Promise
 
 abstract class NtlCheckRootSpec internal constructor(context: ReactApplicationContext) :
   NativeNtlCheckRootSpec(context) {
