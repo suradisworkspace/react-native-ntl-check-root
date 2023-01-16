7eee672e8c3570ffa03e1bdbcdce0ebe5d1c8103
Ratchanan Sakdiyotintada <ratchanan.sak@gmail.com>
2023-01-16 18:40:04 +0700

    fix: minor fix

--- android/src/main/java/com/bootloadercheck/BootloaderCheck.kt
+++ android/src/main/java/com/bootloadercheck/BootloaderCheck.kt
@@ -3,7 +3,6 @@ package com.bootloadercheck
 import com.bootloadercheck.magiskkiller.MagiskKiller.*
 
 class BootloaderCheck {
-    // @SuppressLint("QueryPermissionsNeeded")
     fun isBootloaderUnlocked(): Boolean {
       val result = detectBootloaderProperties()
       return (result and FOUND_BOOTLOADER_UNLOCKED) != 0 || (result and FOUND_BOOTLOADER_SELF_SIGNED) != 0
