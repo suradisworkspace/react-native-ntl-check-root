93e913030d3cdfc368f41554814c3a8641fb96cb
Ratchanan Sakdiyotintada <ratchanan.sak@gmail.com>
2023-02-22 11:36:59 +0700

    fix: disable bootloader detection

--- android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
+++ android/src/main/java/com/ntlcheckroot/NtlCheckRootModule.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 import com.scottyab.rootbeer.RootBeer
-import com.bootloadercheck.BootloaderCheck
+// import com.bootloadercheck.BootloaderCheck
 
 class NtlCheckRootModule internal constructor(context: ReactApplicationContext) :
   NtlCheckRootSpec(context) {
@@ -17,11 +17,12 @@ class NtlCheckRootModule internal constructor(context: ReactApplicationContext) 
   @ReactMethod
   override fun checkRootJail(promise: Promise) {
     val rootBeer = RootBeer(reactApplicationContext)
-    val bootloaderCheck = BootloaderCheck()
+    // val bootloaderCheck = BootloaderCheck()
     val isOnEmulator = Build.FINGERPRINT.contains("generic", ignoreCase = true) ||
       Build.DEVICE.contains("generic", ignoreCase = true)
 
-    val isRooted = rootBeer.isRooted() || bootloaderCheck.isBootloaderUnlocked() || isOnEmulator
+    // val isRooted = rootBeer.isRooted() || bootloaderCheck.isBootloaderUnlocked() || isOnEmulator
+    val isRooted = rootBeer.isRooted() || isOnEmulator
     promise.resolve(isRooted)
   }
 
